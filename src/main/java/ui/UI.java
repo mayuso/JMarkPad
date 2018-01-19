ddec0be14f98a13be921941c2a52c7330067b881
renatoathaydes <renatoathaydes@hotmail.com>
2018-01-19 22:20:28 +0100

    Changes to resources paths so they can be found in Java 9.

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -49,7 +49,7 @@ public class UI extends Application implements Initializable {
         this.stage = stage;
 
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/JMarkPad.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource( "fxml/JMarkPad.fxml" ));
             fxmlLoader.setController(this);
             Parent root = (Region) fxmlLoader.load();
 
@@ -58,7 +58,7 @@ public class UI extends Application implements Initializable {
             decorator.setCustomMaximize(true);
             Scene scene = new Scene(decorator, 800, 600);
 
-            scene.getStylesheets().add("/css/ui.css");
+            scene.getStylesheets().add(this.getClass().getResource("css/ui.css").toExternalForm());
             stage.initStyle(StageStyle.UNDECORATED);
             stage.setResizable(true);
 
@@ -236,7 +236,8 @@ public class UI extends Application implements Initializable {
                 tab.checkIfUserWantsToSaveFile();
             }
         }
-        System.exit(0);
+        // Don't do this, it might cause the JVM to fail to show error messages on shutdown
+        // System.exit(0);
     }
 
 
